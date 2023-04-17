f7cad124724100f93dc36adc87f5c774659d6e4e
D1ZZY <104479520+13511421036@users.noreply.github.com>
2023-04-17 02:37:54 +0000

    Changes to be committed: modified:   homework_1/02_control-flow-statements.dart modified:   homework_1/03_functions.dart new file:   homework_1/04_imports.dart new file:   homework_1/05_class.dart new file:   homework_1/06_enums.dart

--- homework_1/03_functions.dart
+++ homework_1/03_functions.dart
@@ -1,6 +1,7 @@
 void main() {
   var result = fibonacci(20);
   print(result);
+  
 }
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
---
+++ homework_1/04_imports.dart
@@ -0,0 +1,12 @@
+// Importing core libraries
+import 'dart:math';
+
+// Importing libraries from external packages
+//import 'package:test/test.dart';
+
+// Importing files
+import '/workspaces/hello_dart1/homework_1/00_helloworld.dart';
+
+void main(List<String> args) {
+  
+}
